2b55d12415d4ce483abc4382be4c9babcd033e9d
60010915@kmitl.ac.th <60010915@kmitl.ac.th>
2023-02-22 23:14:16 +0000

    update fhv fact

--- models/staging/stg_fhv.sql
+++ models/staging/stg_fhv.sql
@@ -5,7 +5,7 @@
 with tripdata as
 (
   select *,
-    row_number() over(partition by vendorid, pickup_datetime) as rn
+    row_number() over(partition by base_license, pickup_datetime) as rn
   from {{ source('staging','fhv') }}
   where vendorid is not null
 )
@@ -13,7 +13,7 @@ with tripdata as
 select
    -- identifiers
     {{ dbt_utils.surrogate_key(['dispatching_base_num', 'tpep_pickup_datetime']) }} as tripid,
-    --cast(dispatching_base_num as ) as base_license,
+    cast(dispatching_base_num as string) as base_license,
     cast(PUlocationid as integer) as  pickup_locationid,
     cast(DOlocationid as integer) as dropoff_locationid,
 
@@ -24,7 +24,7 @@ select
     -- shared trip
     cast(SR_Flag as integer) as shared_ride_type,
     {{ get_shared_ride_description(sr_flag) }} as shared_ride_description,
-    --cast(Affiliated_base_number as ) as shared_ride_base_license
+    cast(Affiliated_base_number as string) as shared_ride_base_license
 
 from tripdata
 where rn = 1
